b490be7eaf14d144d14a361615ef44adc521a5a0
gopalrohra <gopal.rohra@gmail.com>
2011-03-15 15:09:21 +0530

    only the plugin code

---
+++ lib/gwizard.rb
@@ -0,0 +1,80 @@
+# Gwizard
+module ActionView
+
+module Helpers
+
+module GWizard
+
+def wizard_for(wizard_options={}, html_options={}, &proc)
+concat(tag(:div, html_options,true))
+wizard_steps(wizard_options, &proc)
+concat("</div>")
+end
+
+def wizard_steps(options={}, &proc)
+w=WizardBuilder.new(self, options)
+proc.call(w)
+end
+
+end #GWizard
+
+class WizardBuilder
+include Helpers::TagHelper
+
+      attr_accessor :wizard_title, :binding
+def initialize(binding, options={})
+@wizard_title="Wizard initialize"
+@binding=binding
+@options=options
+end
+
+def step(step_number, html_options={}, &proc)
+current_step=@binding.params[:current_step]==nil ? 1 : @binding.params[:current_step].to_i
+return if current_step!=step_number
+content=@binding.capture(&proc)
+@binding.concat(tag(:div,html_options,true))
+@binding.concat(@binding.content_tag(:div,content,{}))
+insert_buttons_for_navigation(current_step)
+@binding.concat("</div>")
+end
+
+private
+
+def insert_buttons_for_navigation(current_step)
+steps=@options[:steps].to_i
+back_url=generate_back_url(current_step)
+next_url=generate_next_url(current_step)
+finish_url=@options[:finish_url]
+if current_step>1
+@binding.concat(@binding.tag(:input, {:type=>"button", :value=>"Back", :name=>"back", :onclick=>"javascript:gw_back('#{back_url}')"}))
+end
+if current_step<steps
+@binding.concat(@binding.tag(:input, {:type=>"button", :value=>"Next", :name=>"Next", :onclick=>"javascript:gw_next('#{next_url}')"}))
+@binding.concat(@binding.tag(:input, {:type=>"button", :value=>"Skip", :name=>"Skip", :onclick=>"javascript:gw_skip('#{next_url}')"}))
+end
+@binding.concat(@binding.tag(:input, {:type=>"button", :value=>"Finish", :name=>"Finish", :onclick=>"javascript:gw_finish('#{finish_url}')"}))
+end
+
+def generate_back_url(current_step)
+if current_step>1
+@binding.url_for(:overwrite_params=>{:current_step=>current_step-1})
+else
+@binding.request.url
+end
+end
+
+def generate_next_url(current_step)
+steps=@options[:steps]
+if(current_step<steps)
+@binding.url_for(:overwrite_params=>{:current_step=>current_step+1})
+else
+@binding.request.url
+end
+end
+
+end #WizardBuilder
+
+end #Helpers
+
+end #ActionView
+
